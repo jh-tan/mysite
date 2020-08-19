b03791f13729fbb1b9e2c28a11613770e3ef692b
JianHui <hui971105@gmail.com>
2020-08-19 15:24:18 +0800

    Updated Settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -100,8 +100,7 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 #         # 'PORT': '',
 #     }
 # }
-DATABASES['default'] =  dj_database_url.config()
-
+DATABASES = { 'default': dj_database_url.config() }
 
 
 # Password validation
